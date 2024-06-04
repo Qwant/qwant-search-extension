9c5c32d1937e15d5cf4de207bfbf50f41a7564d1
piero <p.vignaux@qwant.com>
2024-06-04 14:44:25 +0200

    Update Firefox search settings for MV3

--- tools/bundle/firefox/manifest.firefox.js
+++ tools/bundle/firefox/manifest.firefox.js
@@ -12,8 +12,8 @@ export const firefoxManifest = {
     search_provider: {
       name: 'Qwant',
       keyword: 'qwant.com',
-      search_url: 'https://www.qwant.com/?q={searchTerms}&client=plgn-firefox-sb',
-      favicon_url: 'https://www.qwant.com/favicon.ico',
+      search_url: 'https://www.qwant.com/?q={searchTerms}&client=ext-firefox-sb', // overrided by brz-moz if other value or additional parameter
+      favicon_url: '/assets/icons/favicon.ico',
       suggest_url: 'https://api.qwant.com/api/suggest/?q={searchTerms}&client=opensearch',
       encoding: 'UTF-8',
       is_default: true,
